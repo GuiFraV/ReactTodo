b7773ecec58a08b16a2956c535816f555eb0003b
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-12-27 13:36:38 +0100

    Add useEffect hooks and local storage functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,71 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./styles.css";
 
-export default function App(){
-
+export default function App() {
   const [newItem, setNewItem] = useState("");
-
   const [todos, setTodos] = useState([]);
 
-  function handleSubmit(e){
+  useEffect(() => {
+    // Retrieve todos from local storage on component mount
+    const storedTodos = localStorage.getItem("todos");
+    if (storedTodos) {
+      setTodos(JSON.parse(storedTodos));
+    }
+  }, []);
+
+  useEffect(() => {
+    // Update local storage whenever todos change
+    if (todos.length > 0) {
+      localStorage.setItem("todos", JSON.stringify(todos));
+    }
+  }, [todos]); 
+
+  function handleSubmit(e) {
     e.preventDefault();
     setTodos((currentTodos) => {
       return [
-        ...currentTodos, 
-        {id: crypto.randomUUID(), 
-          title: newItem, 
-          completed: false}
-      ]
-    })
-
-    setNewItem("")
-    
-  }
-
-  function toggleTodo(id, completed){
-    setTodos(currentTodos => {
-      return currentTodos.map(todo =>{
-        if(todo.id === id){
-          return {...todo, completed}
+        ...currentTodos,
+        {
+          id: crypto.randomUUID(),
+          title: newItem,
+          completed: false
         }
-        return todo
-      })
-    })
+      ];
+    });
+    setNewItem("");
   }
 
-  function deleteTodo(id){
-    setTodos(currentTodos => {
-      return currentTodos.filter((t) => {return id !== t.id})
-    })
+  function toggleTodo(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed };
+        }
+        return todo;
+      });
+    });
   }
 
-  console.log(todos)
+  function deleteTodo(id) {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((t) => {
+        return id !== t.id;
+      });
+    });
+  }
+
+  console.log(todos);
 
   return (
     <>
       <form onSubmit={handleSubmit} className="new-item-form">
         <div className="form-row">
           <label htmlFor="item">New Item</label>
-          <input 
-            value={newItem} 
-            onChange={e => setNewItem(e.target.value)} 
-            type="text"   
-            id="item" 
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            type="text"
+            id="item"
           />
         </div>
         <button className="btn">Add</button>
@@ -59,20 +74,27 @@ export default function App(){
       <h1 className="header">Todo List</h1>
       {todos.length === 0 && <h1>Pas de todos today !</h1>}
       <ul className="list">
-        {todos.map(todo => {
-          return <li key={todo.id}>
-                  <label>
-                    <input 
-                      type="checkbox" 
-                      checked={todo.completed} 
-                      onChange={e => toggleTodo(todo.id, e.target.checked)} 
-                    /> 
-                        {todo.title}
-                  </label>
-                  <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
-                </li>
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={(e) => toggleTodo(todo.id, e.target.checked)}
+                />
+                {todo.title}
+              </label>
+              <button
+                onClick={() => deleteTodo(todo.id)}
+                className="btn btn-danger"
+              >
+                Delete
+              </button>
+            </li>
+          );
         })}
-        </ul>
-    </> 
-  )
+      </ul>
+    </>
+  );
 }
